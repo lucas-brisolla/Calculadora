7ceacdb535577427bca590b7db1ff285e69daab5
lucas-brisolla <99052177+lucas-brisolla@users.noreply.github.com>
2023-04-30 14:08:21 -0300

    primeira versão tentando na marra

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+var display = document.getElementsByClassName("display")
+var close = document.getElementsByID("close")
+var C = document.getElementById("C")
+var bar = document.getElementById("q")
+var dollar = document.getElementById("r")
+var division = document.getElementById("d")
+var seven = document.getElementById("7")
+var eight = document.getElementById("8")
+var nine = document.getElementById("9")
+var times = document.getElementById("m")
+var four = document.getElementById("4")
+var five = document.getElementById("5")
+var six = document.getElementById("6")
+var sub = document.getElementById("s")
+var one = document.getElementById("1")
+var two = document.getElementById("2")
+var three = document.getElementById("3")
+var plus = document.getElementById("p")
+var point = document.getElementById(".")
+var equal = document.getElementById("i")
+var numbers = document.getElementsByClassName("numbers")
+
+numbers.addEventListener("click", num);
+
+function num() {
+  display = ""
+}
+
