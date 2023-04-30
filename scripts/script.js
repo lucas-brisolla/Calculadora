ba559e5fdf9c2542767005695f30600757939185
lucas-brisolla <99052177+lucas-brisolla@users.noreply.github.com>
2023-04-30 15:20:36 -0300

    least update with tutorial

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,29 @@
-var display = document.getElementsByClassName("display")
-var close = document.getElementsByID("close")
-var C = document.getElementById("C")
-var bar = document.getElementById("q")
-var dollar = document.getElementById("r")
-var division = document.getElementById("d")
-var seven = document.getElementById("7")
-var eight = document.getElementById("8")
-var nine = document.getElementById("9")
-var times = document.getElementById("m")
-var four = document.getElementById("4")
-var five = document.getElementById("5")
-var six = document.getElementById("6")
-var sub = document.getElementById("s")
-var one = document.getElementById("1")
-var two = document.getElementById("2")
-var three = document.getElementById("3")
-var plus = document.getElementById("p")
-var point = document.getElementById(".")
-var equal = document.getElementById("i")
-var numbers = document.getElementsByClassName("numbers")
-
-numbers.addEventListener("click", num);
-
-function num() {
-  display = ""
+function insert(num)
+{
+  var numero = document.getElementById('resultado').innerHTML;
+  document.getElementById('resultado').innerHTML = numero + num;
 }
 
+function clean()
+{
+  document.getElementById('resultado').innerHTML = "";
+}
+
+function back()
+{
+  var resultado = document.getElementById('resultado').innerHTML;
+  document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1)
+}
+
+function calc()
+{
+  var resultado = document.getElementById("resultado").innerHTML;
+  if (resultado)
+  {
+    document.getElementById('resultado').innerHTML = eval(resultado);
+  }
+  else
+  {
+    document.getElementById('resultado').innerHTML = "0"
+  }
+}
